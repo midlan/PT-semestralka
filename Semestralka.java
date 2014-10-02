5651cebbfe2569db7e1cc2aa6a23cfd8f88cd709
Milan Davídek <6midlan@gmail.com>
2014-10-02 10:52:25 +0200

    Přidání hlavní třídy

---
+++ Semestralka.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Milan Davídek <midlan@students.zcu.cz>
+ */
+public class Semestralka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
