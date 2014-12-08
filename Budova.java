4a740efe13c4c0e2e753de924c9159691ffa1668
Milan Davídek <6midlan@gmail.com>
2014-12-08 01:39:04 +0100

    Nahrazení místa přímo budovou, úpravy území

--- Budova.java
+++ Budova.java
@@ -2,16 +2,23 @@
  *
  * @author Kate
  */
-public class Budova {
+public abstract class Budova {
 
     public final double x, y;
+    public final String nazev;
 
-    public Budova(Uzemi u, double x, double y) throws SouradniceException {
+    public Budova(String nazev, Uzemi u, double x, double y) {
         
-        if(!u.souradniceUvnitr(x, y))
-            throw new SouradniceException();
+        if(nazev == null) {
+            throw new IllegalArgumentException("Název budovy nemůže být null");
+        }
         
         this.x = x;
         this.y = y;
+        this.nazev = nazev;
+        
+        if (u != null) {
+            u.pridejBudovu(this);
+        }
     }
 }
--- Uzemi.java
+++ Uzemi.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  *
  * @author Kate
@@ -8,20 +11,54 @@ public class Uzemi {
     public final double sirka;
     public final double vyska;
 
+    private final HashSet<Budova> budovy = new HashSet<Budova>();
+
     public Uzemi(double sirka, double vyska) {
         this.sirka = sirka;
         this.vyska = vyska;
     }
 
-    public boolean souradniceUvnitr(double x, double y) {
+    public boolean sourUvnitr(double x, double y) {
         return x >= 0 && y >= 0 && x <= this.sirka && y <= this.vyska;
     }
 
-    public Misto vytvorMisto(double x, double y) throws Exception {
-        return new Misto(x, y, this);
+    public void pridejBudovu(Budova b) {
+
+        if (!this.sourUvnitr(b.x, b.y)) {
+            throw new IllegalArgumentException("Souřadnice budovy jsou mimo toto území");
+        }
+
+        budovy.add(b);
     }
-    
-    public Misto vytvorMisto() throws Exception {
-        return new Misto(Math.random() * this.sirka, Math.random() * this.vyska, this);
+
+    public double vzdalenost(Budova a, Budova b) {
+
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("Jedna z budov je null");
+        }
+
+        if (!budovy.contains(a) || !budovy.contains(b)) {
+            throw new IllegalArgumentException("Jedna z budov není v tomto území.");
+        }
+
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+    }
+
+    public ArrayList<Budova> nejblizsiBudovy(double x, double y, int pocet) {
+
+        if (!this.sourUvnitr(x, y)) {
+            throw new IllegalArgumentException("Souřadnice jsou mimo toto území");
+        }
+        //todo získat budovy
+        return new ArrayList<Budova>();
+    }
+
+    public ArrayList<Budova> nejblizsiBudovy(Budova b, int pocet) {
+
+        if (!this.budovy.contains(b)) {
+            throw new IllegalArgumentException("Budova nepatří do tohoto území");
+        }
+
+        return this.nejblizsiBudovy(b.x, b.y, pocet);
     }
 }
