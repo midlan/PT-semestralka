44cc01db3bd47f071afe2d4e0129c1a692a81b2e
Kateřina Kratochvílová <kata.krat@seznam.cz>
2015-01-06 19:32:04 +0100

    gui ready

--- GUI.java
+++ GUI.java
@@ -1,30 +1,64 @@
-
-import java.awt.HeadlessException;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
 
 /**
  *
  * @author Kate
  */
 public class GUI extends JFrame {
-
-    private final JPanel JPanel = new JPanel();
+    private final JPanel tlacitka = new JPanel();
+    public JTextPane textArea = new JTextPane();
 
     /*
 
      */
     public GUI() {
         this.setTitle("Simulace pivovaru");
-        this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        addButton();
+        addTextArea();
+        this.pack();
+        textArea.setText("ahoj");
     }
     
-    //Semestralka.simulace.metoda();
+    
+    public void addButton(){
+        JButton but1 = new JButton("Dalsi hodina");
+        JButton but2 = new JButton("Konec dne");
+        JButton but3 = new JButton("Na konec simulace");
+        JButton but4 = new JButton("Konec programu");
+        
+        but1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+
+            }
+        });
+        
+        tlacitka.add(but1);
+        this.add(tlacitka, BorderLayout.SOUTH);
+    }
+    
+    public void addTextArea() {
+        textArea.setEditable(false);
+        textArea.setContentType("text/html");
+        JScrollPane scroll = new JScrollPane(textArea);
+        scroll.setPreferredSize(new Dimension(400, 400));
+        this.add(scroll);
+    }
     
     public void Dlaisdwn(){
         Semestralka.simulace.vygenerujObjednavky();
+        
     }
     /*
             this.setPreferredSize(new Dimension(800, 300));
